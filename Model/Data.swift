d31298a54f021b12ad0840a281c1cd00e7c844df
ducndthe153148 <ducndthe153148@fpt.edu.vn>
2021-12-20 10:58:47 +0700

    3

---
+++ Model/Data.swift
@@ -0,0 +1,13 @@
+//
+//  Data.swift
+//  ItineraryApp
+//
+//  Created by Nguyễn Đình Trung Đức on 20/12/2021.
+//
+
+import Foundation
+
+class Data {
+    // array of class to hold data
+    static var tripModels = [TripModel] ()
+}
--- Model/Trip/TripFunction.swift
+++ Model/Trip/TripFunction.swift
@@ -12,8 +12,23 @@ class TripFunction {
         
     }
     
-    static func readTrip(){
+    static func readTrip(completion: @escaping () -> ()){
+        // DispatchQueue: Manages what work occrs on what threads
+        // quality of service (qos) defines priority for thread - .userInteractive: highest priority, .background: lowest priority
+        // async: your app wont wait for you code to finish. it will continue on
         
+        // Giúp người dùng thấy ngay UI mà không phải chờ
+        DispatchQueue.global(qos: .userInteractive).async {
+            if Data.tripModels.count == 0 {
+                Data.tripModels.append(TripModel(title: "Trip to Bali"))
+                Data.tripModels.append(TripModel(title: "Trip to Mexico"))
+                Data.tripModels.append(TripModel(title: "Trip to VietNam"))
+            }
+        }
+        
+        DispatchQueue.main.async {
+            completion()
+        }
     }
     
     static func updateTrip(tripModel: TripModel){
