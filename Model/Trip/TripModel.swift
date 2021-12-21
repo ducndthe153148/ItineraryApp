fe8f6002c27e62e29b825ed880690b35bdc0050f
ducndthe153148 <ducndthe153148@fpt.edu.vn>
2021-12-21 08:40:38 +0700

    .

--- Model/Trip/TripModel.swift
+++ Model/Trip/TripModel.swift
@@ -6,13 +6,16 @@
 //
 
 import Foundation
+import UIKit
 
 class TripModel {
     let id: UUID
     var title: String!
+    var image: UIImage?
     
-    init(title: String){
+    init(title: String, image: UIImage? = nil){
         id = UUID()
         self.title = title
+        self.image = image
     }
 }
